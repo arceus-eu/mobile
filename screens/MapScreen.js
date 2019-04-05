b4255c0c72ddbdac15c09d82f041a9833df9fc5a
Pavel Sanatov <33035511+psanatov@users.noreply.github.com>
2019-04-05 16:53:53 +0200

    Fixed WebView communication with React Native (#2)

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,29 +1,64 @@
 import React from "react";
-import { StyleSheet, WebView, View } from "react-native";
+import { StyleSheet, WebView, View, Text } from "react-native";
 
 export default class MapScreen extends React.Component {
+  state = {
+    someProp: 0
+  };
+
   static navigationOptions = {
     title: "Map"
   };
 
-  const mapHTML = require('./Map/Map.html'); 
+  // Get data from front-end
+  _onMessage(e) {
+    let { data } = e.nativeEvent;
+    const { doPost } = JSON.parse(data);
 
-  postMessage() {
-    this.refs.webview.postMessage(“Hello from RN”);
+    // Setting some property which should go to WebView later
+    this.setState({
+      someProp: Date.now()  // TODO:  for some reason for the first time, 
+                            //        we get 0, instead of a proper timestamp
+    });
+
+    // This is just some 'trigger' I set in Map.html, 
+    // so POST is being triggered when we press GET button
+    // in Map. html 
+    if (doPost === true) {
+      this._postMessage();
+    }
+
+    console.log('Data from WebView: ', data);
   }
 
-  onMessage(message) {
-    console.log(JSON.parse(message.nativeEvent.data));
+  // Post data to front-end
+  _postMessage() {
+    console.log('POST has Started!');
+    // A bit dirty, but it works hehe
+    const func = `(function () {
+    setSomethingOnFrontEnd(${this.state.someProp});
+  })();`;
+
+    this.webview.postMessage(func);
+    console.log('POST has Finished!');
   }
 
   render() {
+    // Static html
+    const mapHTML = require('./Map.html');
+
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
         <WebView
-          ref="webview"
-          source={{ html: this.mapHTML }}
-          onMessage={this.onMessage}
-          style={{ marginTop: 20 }}
+          ref={(x) => { this.webview = x; }}
+
+          source={mapHTML}
+          onMessage={this._onMessage.bind(this)}
+          
+          originWhitelist={['*']}
+          setJavaScriptEnabled={true}
+          javaScriptEnabledAndroid={true}
+          startInLoadingState={true}
         />
       </View>
     );
